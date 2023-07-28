63d97e37a4b3ee66f6fe51d0f405c56f4c660aa6
PrachponleuUch <ponleuvuthy@gmail.com>
2023-07-28 22:57:58 +1000

    added react hooks, functions and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
+import "./style.css"
+import { NewToDoForm } from "./NewToDoForm";
+import { ToDoList } from "./ToDoList";
 
+//! One function can only return one element, it can be solved by using a fragment element (<></>)
+//! To change a state, you need to create a new state instead of mutating it
+//! {() => function()} is recommended to use on most cases so as to prevent unexpected behavior 
+//! Hooks must be at the top and can't be inside any condition or loops (the same hooks must be run every single time)
+//? Inside useState is the default state of newItem unless changed using setNewItem
+//? onChange is used so that the newItem value is changed and the page is rerendered every time an input is made so that it wouldn't remain in its default state(useState) 
+//? Key is needed because it makes it so that react knows what to change what not to (optimized)
+export default function App(){
+  //? State is used to make it more interactive
+  const [ todos, setTodos ] = useState(() => {
+    const localValue = localStorage.getItem('ITEMS')
+    if (localValue === null) return []
+    return JSON.parse(localValue)
+  });
 
-function App() {
-  const [count, setCount] = useState(0)
+  //? UseEffect executes its function every time its 2nd property changes
+  useEffect(() => {
+    localStorage.setItem('ITEM', JSON.stringify(todos))
+  }, [todos])
 
+  function addTodo(title){
+    setTodos((currentTodos) => {
+      return [...currentTodos, {id: crypto.randomUUID(), title, completed: false}]
+    })
+  }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id){
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => {
+        return (todo.id !== id) 
+      })
+    })
+  }
+  
+  //! Use props inside jsx function element to pass function between JSX files
   return (
-    <div className = "App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <> 
+      <NewToDoForm onSubmit={addTodo}/>
+      <h1 className="header">To Do List</h1>
+      <ToDoList 
+        todos={todos}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+      />
+    </>
   )
 }
-
-export default App
